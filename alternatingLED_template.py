44995a83c49bfd4f61f268d63ece392579bc0584
Melrose-FabLab <51517125+Melrose-FabLab@users.noreply.github.com>
2019-06-08 15:36:13 -0400

    Add files via upload

---
+++ alternatingLED_template.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+'''Alternate between two LEDs
+   alternatingLED.py --Frank M.'''
+   
+import RPi.GPIO as GPIO
+import time
+
+pinLED1 = 13
+pinLED2 = 15
+timeDelay = .3  # in seconds
+
+def main():
+
+    print "Starting Script..."
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pinLED1, GPIO.OUT)  #  declare pinLED1 as an output
+    GPIO.setup(pinLED2, GPIO.OUT)  #  declare pinLED2 as an output
+    
+    #Add code here...
+        
+    GPIO.cleanup()
+    print "..................Completed"
+
+if __name__=="__main__":
+    main()
---
+++ basicLED_template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+'''Turn an LED on and off
+   basicLED.py --Frank M.'''
+   
+import RPi.GPIO as GPIO
+import time
+
+pinLED = 13
+timeDelay = .5  # in seconds
+
+def main():
+
+    print "Starting Script..."
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pinLED,GPIO.OUT)  #  declare pinLED as an output
+
+    #Add code here...
+
+    GPIO.cleanup()
+    print "..................Completed"
+
+if __name__=="__main__":
+    main()
